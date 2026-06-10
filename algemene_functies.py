4b7ae91351026474bc522c7086dfe38a0e9cd22b
Frank <frankcattenstart@gmail.com>
2026-06-10 20:49:59 +0200

    opdracht LES 8 afgerond

---
+++ algemene_functies.py
@@ -0,0 +1,14 @@
+def mijn_functie_1(getal):
+    return getal * getal
+
+
+def mijn_functie_2(getal1, getal2):
+    return [
+        getal1 + getal2,
+        getal1 - getal2,
+        getal1 * getal2,
+        getal1 / getal2
+    ]
+
+print(mijn_functie_1(10))
+print(mijn_functie_2(12, 3))
